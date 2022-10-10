9ba48e803cd8ff321d55dd4e989e2de077cd2865
sashanka1 <sashankasekharsamal777@gmail.com>
2022-10-10 17:57:57 +0530

    done modification in aqua controller to get serch query result

--- controllers/aqua.controllers.js
+++ controllers/aqua.controllers.js
@@ -21,26 +21,37 @@ router.post("/", async(req,res)=>{ // add to the data base
 
 router.get("/",async(req,res)=>{  // get all data
     try {
-        let aquap=[];
-        var product_catagory = req.query.pr_ca || "al"
-        if(product_catagory == "fish"){
-             aquap= await Aqua.find({catagory:"fish"});
+        
+        var productName = req.query.pName || "no"
+        if( productName !== "no"){
+           const theproduct = await Aqua.find({name:productName});
+           res.status(200).send(theproduct)
         }
-        else if(product_catagory == "plant"){
-            aquap= await Aqua.find({catagory:"plant"});
-       }
-       else if(product_catagory == "aquarium"){
-        aquap= await Aqua.find({catagory:"aquarium"});
-       }
-      else if(product_catagory == "accessories"){
-        aquap= await Aqua.find({catagory:"accessories"});
-       }
         else{
-            aquap= await Aqua.find({});
+            var aquap=[];
+            var product_catagory = req.query.pr_ca || "al"
+            if(product_catagory == "fish"){
+                 aquap= await Aqua.find({catagory:"fish"});
+            }
+            else if(product_catagory == "plant"){
+                aquap= await Aqua.find({catagory:"plant"});
+           }
+           else if(product_catagory == "aquarium"){
+            aquap= await Aqua.find({catagory:"aquarium"});
+           }
+          else if(product_catagory == "accessories"){
+            aquap= await Aqua.find({catagory:"accessories"});
+           }
+            else{
+                aquap= await Aqua.find({});
+            }
+
+            res.status(200).send(aquap);
         }
        
+       
         
-        res.send(aquap);
+    
     } catch (error) {
         res.status(500).send(error);
     }
